3d23bf1bb44aeb55b4a6c8f04af5286248fe330c
kimkyunghun3 <kimkyunghun3@naver.com>
2020-07-23 16:00:56 +0900

    Entity에 동일하게 Repository 모두 생성

--- src/main/java/com/example/study/model/repository/UserRepository.java
+++ src/main/java/com/example/study/model/repository/UserRepository.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    // select * from user where account = ? << test03, test04
-    Optional<User> findByAccount(String account); //JPA 에서는 Select문 인것을 findBy를 보고 알게 되고 account와 매칭을 시켜 account에 들어가는 값이 들어간다.
-
-    Optional<User> findByEmail(String email);
-
-
-    // select * fro, user where account = ? and email = ?
-    Optional<User> findByAccountAndEmail(String account, String email);
+//
+//    // select * from user where account = ? << test03, test04
+//    Optional<User> findByAccount(String account); //JPA 에서는 Select문 인것을 findBy를 보고 알게 되고 account와 매칭을 시켜 account에 들어가는 값이 들어간다.
+//
+//    Optional<User> findByEmail(String email);
+//
+//
+//    // select * fro, user where account = ? and email = ?
+//    Optional<User> findByAccountAndEmail(String account, String email);
 }
